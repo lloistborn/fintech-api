4398716d2a3957edd7e091c2bb83111a65f1d8e0
morensya <faisalmorensya@gmail.com>
2024-03-16 11:04:24 +0400

    Introduce Extension attribute

--- src/main/java/com/fintech/webapi/domain/MoneyExtension.java
+++ src/main/java/com/fintech/webapi/domain/MoneyExtension.java
@@ -1,6 +1,13 @@
 package com.fintech.webapi.domain;
 
+import java.util.Currency;
 
+import manifold.ext.rt.api.Extension;
+import manifold.ext.rt.api.This;
+
+@Extension
 public class MoneyExtension {
-    
+    public static Currency GetCurrency(@This Money money) {
+        return money.GetCurrency();
+    }
 }
